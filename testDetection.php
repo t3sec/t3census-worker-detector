07d932672a1f17a97e27089149d22a6796dcb88e
Marcus Krause <marcus@t3sec.info>
2017-02-03 21:38:58 +0100

    [FEATURE] Use composer package t3sec/typo3cms-detection instead of library code

--- testDetection.php
+++ testDetection.php
@@ -1,29 +1,21 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
-$dir = dirname(__FILE__);
-$libraryDir = realpath($dir . '/library');
-
-require_once $libraryDir . '/Detection/Context.php';
-require_once $libraryDir . '/Detection/Request.php';
-require_once $libraryDir . '/Detection/Normalization/RedirectProcessor.php';
-require_once $libraryDir . '/Detection/Identification/HostOnlyProcessor.php';
-require_once $libraryDir . '/Detection/Identification/FullPathProcessor.php';
-require_once $libraryDir . '/Detection/Normalization/ShortenerRedirectOnlyProcessor.php';
-require_once $libraryDir . '/Detection/Identification/Typo3ArtefactsProcessor.php';
-require_once $libraryDir . '/Detection/Classification/ExistingRequestsProcessor.php';
-require_once $libraryDir . '/Detection/Classification/HostOnlyProcessor.php';
-require_once $libraryDir . '/Detection/Classification/FullPathProcessor.php';
-require_once $libraryDir . '/Detection/Classification/Typo3ArtefactsProcessor.php';
-require_once $libraryDir . '/Detection/Classification/Typo3FingerprintProcessor.php';
+use T3sec\Typo3Cms\Detection\Context;
+use T3sec\Typo3Cms\Detection\Classification\Typo3FingerprintProcessor;
+use T3sec\Typo3Cms\Detection\Identification\FullPathProcessor;
+use T3sec\Typo3Cms\Detection\Identification\HostOnlyProcessor;
+use T3sec\Typo3Cms\Detection\Identification\Typo3ArtefactsProcessor;
+use T3sec\Typo3Cms\Detection\Normalization\RedirectProcessor;
+use T3sec\Typo3Cms\Detection\Normalization\ShortenerRedirectOnlyProcessor;
 
 /*
-$foo = new \T3census\Detection\Context();
-$bar = new \T3census\Detection\Request();
+$foo = new Context();
+$bar = new Request();
 $foo->addRequest($bar);
 print_r($foo->getRequest());
 */
-$context = new \T3census\Detection\Context();
+$context = new Context();
 $context->setUrl('http://www.typovision.de/de/agentur/aktivitaeten/');
 $context->setUrl('http://kamerakind.net/');
 $context->setUrl('http://bit.ly/nYswn2');
@@ -40,39 +32,28 @@ $context->setUrl('http://www.foreverknowledge.info');
 $context->setUrl('https://storm.torproject.org');
 
 
-$objTypo3Artefacts = new \T3census\Detection\Identification\Typo3ArtefactsProcessor(NULL, TRUE);
-$objPathRedirect = new \T3census\Detection\Identification\FullPathProcessor($objTypo3Artefacts, TRUE);
-$objPathNoRedirect = new \T3census\Detection\Identification\FullPathProcessor($objPathRedirect, FALSE);
-$objHostRedirect = new \T3census\Detection\Identification\HostOnlyProcessor($objPathNoRedirect, TRUE);
-$objHostNoRedirect = new \T3census\Detection\Identification\HostOnlyProcessor($objHostRedirect, FALSE);
-$objRedirect = new \T3census\Detection\Normalization\RedirectProcessor($objHostNoRedirect);
-$objShortener = new \T3census\Detection\Normalization\ShortenerRedirectOnlyProcessor($objRedirect);
+$objTypo3Artefacts = new Typo3ArtefactsProcessor(NULL, TRUE);
+$objPathRedirect = new FullPathProcessor($objTypo3Artefacts, TRUE);
+$objPathNoRedirect = new FullPathProcessor($objPathRedirect, FALSE);
+$objHostRedirect = new HostOnlyProcessor($objPathNoRedirect, TRUE);
+$objHostNoRedirect = new HostOnlyProcessor($objHostRedirect, FALSE);
+$objRedirect = new RedirectProcessor($objHostNoRedirect);
+$objShortener = new ShortenerRedirectOnlyProcessor($objRedirect);
 $objShortener->process($context);
 
 
-//$objRedirect = new \T3census\Detection\Normalization\RedirectProcessor(NULL);
+//$objRedirect = RedirectProcessor(NULL);
 //$objRedirect->process($context);
 
 print_r($context);
 
 
 if (TRUE || is_bool($context->getIsTypo3Cms()) && $context->getIsTypo3Cms()) {
-	/*
-		$objArtefacts = new \T3census\Detection\Classification\Typo3ArtefactsProcessor();
-		$objFullPath = new \T3census\Detection\Classification\FullPathProcessor($objArtefacts);
-		$objHost = new \T3census\Detection\Classification\HostOnlyProcessor($objFullPath);
-		$objRequest = new \T3census\Detection\Classification\ExistingRequestsProcessor($objHost);
-		$objRequest->process($context);
-	*/
-	$objFingerprint = new \T3census\Detection\Classification\Typo3FingerprintProcessor();
+	$objFingerprint = new Typo3FingerprintProcessor();
 	$objFingerprint->process($context);
 	unset($objRequest);
 }
 
 
-/*
-$objShortener = new \T3census\Detection\Identification\ShortenerRedirectOnlyProcessor();
-$objShortener->process($context);
-*/
 print_r($context);
 ?>
--- worker/Typo3HostDetectorWorker.php
+++ worker/Typo3HostDetectorWorker.php
@@ -1,27 +1,12 @@
 <?php
-$dir = dirname(__FILE__);
-$libraryDir = realpath($dir . '/../library');
-$vendorDir = realpath($dir . '/../vendor');
-
-require_once $libraryDir . '/Detection/Context.php';
-require_once $libraryDir . '/Detection/Request.php';
-require_once $libraryDir . '/Detection/Normalization/ShortenerRedirectOnlyProcessor.php';
-require_once $libraryDir . '/Detection/Normalization/RedirectProcessor.php';
-require_once $libraryDir . '/Detection/Identification/HostOnlyProcessor.php';
-require_once $libraryDir . '/Detection/Identification/FullPathProcessor.php';
-require_once $libraryDir . '/Detection/Identification/Typo3ArtefactsProcessor.php';
-require_once $libraryDir . '/Detection/Classification/ExistingRequestsProcessor.php';
-require_once $libraryDir . '/Detection/Classification/HostOnlyProcessor.php';
-require_once $libraryDir . '/Detection/Classification/FullPathProcessor.php';
-require_once $libraryDir . '/Detection/Classification/Typo3ArtefactsProcessor.php';
-require_once $libraryDir . '/Detection/Classification/Typo3FingerprintProcessor.php';
-require_once $vendorDir . '/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\GelfHandler;
 use Gelf\Publisher;
 use Gelf\Transport\UdpTransport;
+use T3sec\Typo3Cms\Detection\Context;
 
 $logfile = __DIR__ . '/../t3census-worker-detector.log';
 
@@ -62,27 +47,27 @@ function fetchUrl(GearmanJob $job) {
 
 	$result = array();
 
-	$context = new \T3census\Detection\Context();
+	$context = new Context();
 	$context->setUrl($job->workload());
 
 	$logger->addDebug('Processing URL', array('url' => $job->workload()));
 
-	$objTypo3Artefacts = new \T3census\Detection\Identification\Typo3ArtefactsProcessor(NULL, TRUE);
-	$objPathRedirect = new \T3census\Detection\Identification\FullPathProcessor($objTypo3Artefacts, TRUE);
-	$objPathNoRedirect = new \T3census\Detection\Identification\FullPathProcessor($objPathRedirect, FALSE);
-	$objHostRedirect = new \T3census\Detection\Identification\HostOnlyProcessor($objPathNoRedirect, TRUE);
-	$objHostNoRedirect = new \T3census\Detection\Identification\HostOnlyProcessor($objHostRedirect, FALSE);
-	$objRedirect = new \T3census\Detection\Normalization\RedirectProcessor($objHostNoRedirect);
-	$objShortener = new \T3census\Detection\Normalization\ShortenerRedirectOnlyProcessor($objRedirect);
+	$objTypo3Artefacts = new T3sec\Typo3Cms\Detection\Identification\Typo3ArtefactsProcessor(NULL, TRUE);
+	$objPathRedirect = new T3sec\Typo3Cms\Detection\Identification\FullPathProcessor($objTypo3Artefacts, TRUE);
+	$objPathNoRedirect = new T3sec\Typo3Cms\Detection\Identification\FullPathProcessor($objPathRedirect, FALSE);
+	$objHostRedirect = new T3sec\Typo3Cms\Detection\Identification\HostOnlyProcessor($objPathNoRedirect, TRUE);
+	$objHostNoRedirect = new T3sec\Typo3Cms\Detection\Identification\HostOnlyProcessor($objHostRedirect, FALSE);
+	$objRedirect = new T3sec\Typo3Cms\Detection\Normalization\RedirectProcessor($objHostNoRedirect);
+	$objShortener = new T3sec\Typo3Cms\Detection\Normalization\ShortenerRedirectOnlyProcessor($objRedirect);
 	$objShortener->process($context);
 	unset($objShortener, $objHostNoRedirect, $objHostNoRedirect, $objHostRedirect, $objPathNoRedirect, $objPathRedirect);
 
 	if (is_bool($context->getIsTypo3Cms()) && $context->getIsTypo3Cms()) {
-		$objFingerprint = new \T3census\Detection\Classification\Typo3FingerprintProcessor();
-		$objArtefacts = new \T3census\Detection\Classification\Typo3ArtefactsProcessor($objFingerprint);
-		$objFullPath = new \T3census\Detection\Classification\FullPathProcessor($objArtefacts);
-		$objHost = new \T3census\Detection\Classification\HostOnlyProcessor($objFullPath);
-		$objRequest = new \T3census\Detection\Classification\ExistingRequestsProcessor($objHost);
+		$objFingerprint = new T3sec\Typo3Cms\Detection\Classification\Typo3FingerprintProcessor();
+		$objArtefacts = new T3sec\Typo3Cms\Detection\Classification\Typo3ArtefactsProcessor($objFingerprint);
+		$objFullPath = new T3sec\Typo3Cms\Detection\Classification\FullPathProcessor($objArtefacts);
+		$objHost = new T3sec\Typo3Cms\Detection\Classification\HostOnlyProcessor($objFullPath);
+		$objRequest = new T3sec\Typo3Cms\Detection\Classification\ExistingRequestsProcessor($objHost);
 		$objRequest->process($context);
 		unset($objRequest, $objHost);
 	}
